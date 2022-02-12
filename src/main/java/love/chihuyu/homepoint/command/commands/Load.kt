d0362d4e4fdfad5f23ba2cbffb96f4dad12aca31
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2022-02-12 16:49:56 +0900

    add: save and load

--- src/main/java/love/chihuyu/homepoint/command/commands/Load.kt
+++ src/main/java/love/chihuyu/homepoint/command/commands/Load.kt
@@ -1,11 +1,22 @@
 package love.chihuyu.homepoint.command.commands
 
+import love.chihuyu.homepoint.PointDatas
 import love.chihuyu.homepoint.command.Command
 import org.bukkit.command.CommandSender
 
 object Load: Command("loadhome") {
     override fun onCommand(sender: CommandSender, label: String, args: Array<out String>) {
+        var err: String? = null
+        if (!sender.hasPermission("homepoint.command.savehome")) {
+            err = "You don't have permission."
+        }
+        if (err != null) {
+            sender.sendMessage("§c$err")
+            return
+        }
 
+        PointDatas.load()
+        sender.sendMessage("§7Data was loaded successfully.")
     }
 
     override fun onTabComplete(sender: CommandSender, label: String, args: Array<out String>): List<String>? {
--- src/main/java/love/chihuyu/homepoint/command/commands/Save.kt
+++ src/main/java/love/chihuyu/homepoint/command/commands/Save.kt
@@ -1,11 +1,22 @@
 package love.chihuyu.homepoint.command.commands
 
+import love.chihuyu.homepoint.PointDatas
 import love.chihuyu.homepoint.command.Command
 import org.bukkit.command.CommandSender
 
 object Save: Command("savehome") {
     override fun onCommand(sender: CommandSender, label: String, args: Array<out String>) {
-        TODO("Not yet implemented")
+        var err: String? = null
+        if (!sender.hasPermission("homepoint.command.savehome")) {
+            err = "You don't have permission."
+        }
+        if (err != null) {
+            sender.sendMessage("§c$err")
+            return
+        }
+
+        PointDatas.save()
+        sender.sendMessage("§7Data was saved successfully.")
     }
 
     override fun onTabComplete(sender: CommandSender, label: String, args: Array<out String>): List<String>? {
