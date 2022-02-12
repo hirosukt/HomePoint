ad74efb4a63f53dbacd8bfbc9b0f91742d4fa5b4
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2022-02-12 16:53:09 +0900

    add: listhome

--- src/main/java/love/chihuyu/homepoint/command/commands/ListHome.kt
+++ src/main/java/love/chihuyu/homepoint/command/commands/ListHome.kt
@@ -1,12 +1,29 @@
 package love.chihuyu.homepoint.command.commands
 
+import love.chihuyu.homepoint.PointDatas
 import love.chihuyu.homepoint.command.Command
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 
-object ListHome: Command("listhome") {
+object ListHome : Command("listhome") {
     override fun onCommand(sender: CommandSender, label: String, args: Array<out String>) {
-        if (sender !is Player || !sender.hasPermission("homepoint.command.listhome")) return
+        var err: String? = null
+        if (sender !is Player) {
+            err = "Please do command in world."
+        } else if (!sender.hasPermission("homepoint.command.listhome")) {
+            err = "You don't have permission."
+        } else if (args.isEmpty()) {
+            err = "Please input name."
+        }
+        if (err != null) {
+            sender.sendMessage("§c$err")
+            return
+        }
+
+        val player =
+            PointDatas.points[(sender as Player).uniqueId]?.joinToString("\n") { "§6${it.pointName} §7-> ${it.location.x}, ${it.location.y}, ${it.location.z}" }
+        sender.sendMessage("§7Here are your home list.")
+        sender.sendMessage(player)
     }
 
     override fun onTabComplete(sender: CommandSender, label: String, args: Array<out String>): List<String>? {
