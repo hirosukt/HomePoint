6904e6541bdd33ce15a727770a0b2c078f670d32
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2022-02-12 16:53:18 +0900

    add: variable visibility

--- src/main/java/love/chihuyu/homepoint/point/Point.kt
+++ src/main/java/love/chihuyu/homepoint/point/Point.kt
@@ -3,7 +3,7 @@ package love.chihuyu.homepoint.point
 import org.bukkit.Location
 import org.bukkit.configuration.serialization.ConfigurationSerializable
 
-class Point(val pointName: String, private val location: Location): ConfigurationSerializable {
+class Point(val pointName: String, val location: Location): ConfigurationSerializable {
 
     override fun serialize(): MutableMap<String, Any> {
         val map = hashMapOf<String, Any>()
